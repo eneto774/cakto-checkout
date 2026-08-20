2d93968fd78d7cc54d629a3c2abe0aa05914995f
Edgar Ribeiro <eneto774@gmail.com>
2026-08-20 00:19:16 -0300

    feat: structure base for layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-
 export default async function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      this is the home page
+    <div className="min-h-screen bg-gray-100 py-6 font-sans">
+      <main className="mx-auto flex w-full max-w-md flex-col gap-5 px-4">
+        <section className="flex flex-col items-center justify-center gap-4 rounded-lg bg-white p-6 shadow-md">
+          Course Information
+        </section>
+        <section className="flex flex-col items-center justify-center gap-4 rounded-lg bg-white p-6 shadow-md">
+          Personal Data
+        </section>
+        <section className="flex flex-col items-center justify-center gap-4 rounded-lg bg-white p-6 shadow-md">
+          Payment Method
+        </section>
+        <section className="flex flex-col items-center justify-center gap-4 rounded-lg bg-white p-6 shadow-md">
+          Resume
+        </section>
+        <section className="flex flex-col items-center justify-center gap-4 rounded-lg bg-white p-6 shadow-md">
+          CTA
+        </section>
+      </main>
     </div>
   );
 }
